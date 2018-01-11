1f97720c5e5191e6444b6851f0beb538da58b415
Keith Smith <32456218+afewvowels@users.noreply.github.com>
2018-01-11 14:49:07 -0500

    Empty application created.

---
+++ src/java_prg_06_03_personal_information_class/JAVA_PRG_06_03_Personal_Information_Class.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package java_prg_06_03_personal_information_class;
+
+/**
+ *
+ * @author bluebackdev
+ */
+public class JAVA_PRG_06_03_Personal_Information_Class {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
